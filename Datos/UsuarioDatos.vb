a1a1dddd7b967ea2348658faeb5f3838d84afa03
evictorero <evictorero@gmail.com>
2016-08-23 22:36:34 -0300

    commit test

---
+++ Datos/UsuarioDatos.vb
@@ -0,0 +1,52 @@
+﻿Imports Datos.ProveedorDeDatos
+
+Imports DTO
+
+Public Class UsuarioDatos
+    Private Shared ProximoId As Integer
+    Public Shared Function Obtener(ByVal pId As Integer) As DTO.UsuarioDTO
+        If pId > 0 Then
+            Dim mDs As DataSet = DB.ExecuteDataset("SELECT id, nombre, apellido FROM dbo.busuario WHERE id_usuario = " & pId)
+            If Not IsNothing(mDs) AndAlso mDs.Tables.Count > 0 AndAlso mDs.Tables(0).Rows.Count > 0 Then
+                Dim mDTO As New DTO.UsuarioDTO
+
+                CargarDTO(mDTO, mDs.Tables(0).Rows(0))
+
+                Return mDTO
+            Else
+
+                Throw New ApplicationException("Fallo al cargar el Usuario.")
+                Return Nothing
+            End If
+        Else
+            Throw New ApplicationException("Se intentó cargar el Usuario sin Id especificado")
+            Return Nothing
+        End If
+
+    End Function
+    Private Shared Sub CargarDTO(ByVal pDTO As DTO.UsuarioDTO, ByVal pDr As DataRow)
+        pDTO.id = pDr("id_usuario")
+        pDTO.usuario = pDr("usuario")
+        pDTO.nombre = pDr("nombre")
+        pDTO.apellido = pDr("apellido")
+        pDTO.email = pDr("email")
+        pDTO.dni = pDr("dni")
+        ' probando 
+        pDTO.fechaNacimiento = pDr("fecha_nacimiento")
+    End Sub
+    Public Shared Function Listar() As List(Of DTO.UsuarioDTO)
+
+        Dim mCol As New List(Of DTO.UsuarioDTO)
+        Dim mDs As DataSet = DB.ExecuteDataset(("SELECT id_usuario, nombre, apellido, email, dni, fecha_nacimiento FROM dbo.busuario "))
+
+        For Each mDr As DataRow In mDs.Tables(0).Rows
+            Dim mDTO As New UsuarioDTO
+
+            CargarDTO(mDTO, mDr)
+
+            mCol.Add(mDTO)
+        Next
+
+        Return mCol
+    End Function
+End Class
---
+++ Negocio/Usuario.vb
@@ -0,0 +1,103 @@
+﻿Imports Microsoft.VisualBasic
+Imports DTO.UsuarioDTO
+Imports Datos.ProveedorDeDatos
+
+
+Namespace Negocio
+
+    Public Class Usuario
+
+#Region "Declaraciones"
+
+        Dim mUombre As String
+        Dim mNombre As String
+        Dim mApellido As String
+        Dim mDNI As Integer
+        Dim mFechaNacimiento As Date
+        Dim mEmail As String
+        Private Shared ProximoId As Integer
+        Dim mId As Integer = 0
+
+#End Region
+
+#Region "Constructores"
+        Public Sub New(ByVal pId As Integer)
+            mId = pId
+            Cargar()
+        End Sub
+        Public Sub New(ByVal pDr As DataRow)
+            Me.Cargar(pDr)
+        End Sub
+        Public Sub New(ByVal pDTO As DTO.UsuarioDTO)
+            Me.Cargar(pDTO)
+        End Sub
+#End Region
+#Region "Métodos"
+
+        Public Overridable Sub Cargar()
+            If mId > 0 Then
+                Dim mDTO As DTO.UsuarioDTO = Datos.UsuarioDatos.Obtener(mId)
+                MyClass.Cargar(mDTO)
+            Else
+                Throw New ApplicationException("Se intentó cargar un Usuario sin Id especificado")
+
+            End If
+        End Sub
+
+        Public Overridable Sub Cargar(ByVal pDr As DataRow)
+            Try
+                mId = pDr("id")
+                mNombre = pDr("nombre")
+                mApellido = pDr("apellido")
+                mDNI = pDr("apellido")
+                mFechaNacimiento = pDr("fecha_nacimiento")
+                mEmail = pDr("email")
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+
+        End Sub
+        Public Overridable Sub Cargar(ByVal pId As Integer)
+            Try
+                mId = pId
+                MyClass.Cargar()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+
+        End Sub
+        Public Sub Cargar(ByVal pDTO As DTO.UsuarioDTO)
+            mId = pDTO.Id
+            mNombre = pDTO.Nombre
+            mApellido = pDTO.Apellido
+        End Sub
+
+        Private Shared Function ObtenerProximoId() As Integer
+            If ProximoId = 0 Then
+                Dim mTempId As Object = DB.ObtenerId("busuario")
+                If mTempId Is DBNull.Value Then
+                    ProximoId = 0
+                Else
+                    ProximoId = mTempId
+                End If
+            End If
+            ProximoId += 1
+            Return ProximoId
+        End Function
+
+        Public Overridable Function Listar() As Collections.Generic.List(Of Usuario)
+            Dim mCol As New Collections.Generic.List(Of Usuario)
+            Dim mColDTO As List(Of DTO.UsuarioDTO) = Datos.UsuarioDatos.Listar()
+
+            For Each mDTO As DTO.UsuarioDTO In mColDTO
+                mCol.Add(New Negocio.Usuario(mDTO))
+            Next
+
+            Return mCol
+        End Function
+#End Region
+
+    End Class
+
+
+End Namespace
