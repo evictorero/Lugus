d1cda4f08615c2f766262d1e031909e5c20e7672
evictorero <evictorero@gmail.com>
2016-08-23 22:50:33 -0300

    agrego UsuarioDTO

--- Interfaz/Usuario.vb
+++ Interfaz/Usuario.vb
@@ -13,7 +13,7 @@
     End Sub
 
     Private Sub Usuario_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Me.cbId_Propietario.DataSource = (New Negocio.Negocio.Propietario).Listar
+        'Me.cbId_Propietario.DataSource = (New Negocio.Negocio.Propietario).Listar
 
 
 
---
+++ Usuario.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic
+Imports DTO.UsuarioDTO
+Imports DB.ProveedorDeDatos
+
+Namespace Negocio
+
+#Region "Declaraciones"
+
+    Dim mApellido As String
+    Dim mNombre As String
+    Private Shared ProximoId As Integer
+    Dim mId As Integer = 0
+
+#End Region
+
+    Public Class Usuario
+
+    End Class
+
+
+End Namespace
